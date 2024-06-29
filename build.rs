fad527ffc3136349dd23660948c9193b2ab146e9
kutoru <eg_bls@mail.ru>
2024-06-29 16:36:49 +0300

    Remove client from tonic build

--- build.rs
+++ build.rs
@@ -1,9 +1,14 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
+        .build_client(false)
         .build_server(true)
         .compile(
-            &["./proto/notes.proto", "./proto/tags.proto", "./proto/files.proto"],
+            &[
+                "./proto/notes.proto",
+                "./proto/tags.proto",
+                "./proto/files.proto",
+            ],
             &["proto"],
         )?;
 
