910cee0ffc87ba67e83bba3a474c3abba8381593
kutoru <eg_bls@mail.ru>
2024-07-01 22:43:07 +0300

    Add crud routes for tags

--- src/server/tags.rs
+++ src/server/tags.rs
@@ -1,8 +1,8 @@
 use crate::proto::tags::tags_server::{Tags, TagsServer};
 use crate::proto::tags::{CreateTagReq, ReadTagsReq, UpdateTagReq, DeleteTagReq, Tag, TagList, Empty};
-use crate::types::{AppState, ServiceResult};
+use crate::types::{AppState, HandleServiceError, ServiceResult};
 
-use tonic::{Request, Response, Status};
+use tonic::{Request, Response};
 
 pub fn get_service(state: AppState) -> TagsServer<AppState> {
     TagsServer::new(state)
@@ -14,27 +14,84 @@ impl Tags for AppState {
         &self,
         request: Request<CreateTagReq>,
     ) -> ServiceResult<Tag> {
-        Err(Status::unimplemented(""))
+
+        let req_body = request.into_inner();
+
+        let new_tag = sqlx::query_as::<_, Tag>("INSERT INTO tags (user_id, name) VALUES ($1, $2) RETURNING *;")
+            .bind(req_body.user_id).bind(req_body.name)
+            .fetch_one(&self.pool)
+            .await
+            .map_to_status()?;
+
+        Ok(Response::new(new_tag))
     }
 
     async fn read_tags(
         &self,
         request: Request<ReadTagsReq>,
     ) -> ServiceResult<TagList> {
-        Err(Status::unimplemented(""))
+
+        let req_body = request.into_inner();
+
+        let tags = sqlx::query_as::<_, Tag>("SELECT * FROM tags WHERE user_id = $1 ORDER BY id;")
+            .bind(req_body.user_id)
+            .fetch_all(&self.pool)
+            .await
+            .map_to_status()?;
+
+        Ok(Response::new(TagList { tags }))
     }
 
     async fn update_tag(
         &self,
         request: Request<UpdateTagReq>,
     ) -> ServiceResult<Tag> {
-        Err(Status::unimplemented(""))
+
+        let req_body = request.into_inner();
+
+        let updated_tag = sqlx::query_as::<_, Tag>("UPDATE tags SET name = $1 WHERE id = $2 AND user_id = $3 RETURNING *;")
+            .bind(req_body.name).bind(req_body.id).bind(req_body.user_id)
+            .fetch_one(&self.pool)
+            .await
+            .map_to_status()?;
+
+        Ok(Response::new(updated_tag))
     }
 
     async fn delete_tag(
         &self,
         request: Request<DeleteTagReq>,
     ) -> ServiceResult<Empty> {
-        Err(Status::unimplemented(""))
+
+        let req_body = request.into_inner();
+
+        let mut transaction = self.pool
+            .begin()
+            .await
+            .map_to_status()?;
+
+        sqlx::query("DELETE FROM note_tags WHERE tag_id = $1;")
+            .bind(req_body.id)
+            .execute(&mut *transaction)
+            .await
+            .map_to_status()?;
+
+        sqlx::query("DELETE FROM tags WHERE id = $1 AND user_id = $2;")
+            .bind(req_body.id).bind(req_body.user_id)
+            .execute(&self.pool)
+            .await
+            .map_to_status()?
+            .rows_affected()
+            .eq(&1)
+            .then_some(())
+            .ok_or(sqlx::Error::RowNotFound)
+            .map_to_status()?;
+
+        transaction
+            .commit()
+            .await
+            .map_to_status()?;
+
+        Ok(Response::new(Empty {}))
     }
 }
